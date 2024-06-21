3f91abe73353064b33053afca8439ff15a028dc5
Vedant Mali <vedantmali05@gmail.com>
2024-06-21 22:27:09 +0530

    AVS Calculation Page structured

--- js/index.js
+++ js/index.js
@@ -77,22 +77,43 @@ document.addEventListener("DOMContentLoaded", () => {
         SIDEBARs OPENING AND CLOSING
     /////////////// */
 
+    // Input Aside
     let asideElementsArray = [document.querySelector(".input-aside-open-btn"), document.querySelector(".input-aside-close-btn"), document.querySelector(".input-aside")];
 
     // Input Aside Open Button Click
     asideElementsArray[0].addEventListener("click", () => {
         asideElementsArray.forEach(elem => elem.classList.add("aside-visible"));
-    })
+    });
 
     // Input Aside Close Button Click
     asideElementsArray[1].addEventListener("click", () => {
         asideElementsArray.forEach(elem => elem.classList.remove("aside-visible"));
-    })
+    });
 
     // Input Aside Scrim Click - Close Aside
     asideElementsArray[2].addEventListener('click', (e) => {
         if (!e.target.closest(".aside-body")) {
             asideElementsArray.forEach(elem => elem.classList.remove("aside-visible"));
         }
-    })
+    });
+
+    // Navigation bar
+    let navElementArray = [document.querySelector(".nav-open-btn"), document.querySelector(".nav-close-btn"), document.querySelector("nav")];
+
+    // Navigation bar Open Button Click
+    navElementArray[0].addEventListener("click", () => {
+        navElementArray.forEach(elem => elem.classList.add("nav-visible"));
+    });
+
+    // Navigation bar Close Button Click
+    navElementArray[1].addEventListener("click", () => {
+        navElementArray.forEach(elem => elem.classList.remove("nav-visible"));
+    });
+
+    // Navigation bar Scrim Click - Close nav
+    navElementArray[2].addEventListener('click', (e) => {
+        if (!e.target.closest(".nav-body")) {
+            navElementArray.forEach(elem => elem.classList.remove("nav-visible"));
+        }
+    });
 });
